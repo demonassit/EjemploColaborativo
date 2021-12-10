4d9a982111adedf0852e22a46865e9f27c46cb2a
demonassit <jminorg@ipn.mx>
2021-12-10 10:23:17 -0600

    Ejemplo colaborativo

---
+++ 16_EjemploColaborativo/src/main/java/com/mycompany/_ejemplocolaborativo/Persona.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany._ejemplocolaborativo;
+
+/**
+ *
+ * @author demon
+ */
+public class Persona {
+    
+    private int id;
+    private String nombre, appat, appmat;
+
+    public Persona(){
+    }
+    
+    public Persona(int id, String nombre, String appat, String appmat) {
+        this.id = id;
+        this.nombre = nombre;
+        this.appat = appat;
+        this.appmat = appmat;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAppat() {
+        return appat;
+    }
+
+    public void setAppat(String appat) {
+        this.appat = appat;
+    }
+
+    public String getAppmat() {
+        return appmat;
+    }
+
+    public void setAppmat(String appmat) {
+        this.appmat = appmat;
+    }
+    
+    
+    
+}
